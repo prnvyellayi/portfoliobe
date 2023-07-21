3243dbaa4ab6993305f3a1ad4470460ba70aea2c
prnvyellayi <prnvshanmukh00@gmail.com>
2023-07-21 23:04:15 +0530

    chatbot trained

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { ConversationalRetrievalQAChain } = require("langchain/chains");
+const { BufferMemory } = require('langchain/memory')
+const { ChatOpenAI } = require("langchain/chat_models/openai");
+const { OpenAIEmbeddings } = require('langchain/embeddings/openai')
+const { MemoryVectorStore } = require('langchain/vectorstores/memory')
+const { TextLoader } = require('langchain/document_loaders/fs/text')
+const { RecursiveCharacterTextSplitter } = require('langchain/text_splitter')
 require('dotenv').config();
 
-const { Configuration, OpenAIApi } = require('openai')
-
-const config = new Configuration({
-    apiKey: process.env.GPT_API_KEY,
-    organization: process.env.GPT_ORG_KEY
-})
-
-const openai = new OpenAIApi(config)
+const memory = new BufferMemory({
+    memoryKey: "chat_history",
+    returnMessages: true,
+});
 //server
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-//endpoint chatGPT
-var messages = []
+// //endpoint chatGPT
 app.post('/chat', async (req, res) => {
-    const msg = req.body.msg;
-    messages.push(msg)
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-        max_tokens: 512,
+    const msg = req.body.msg.content;
+    // console.log(msg)
+    
+    const loader = new TextLoader('./resume.txt')
+    const docs = await loader.load()
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 0,
+    });
+
+    const splitDocs = await textSplitter.splitDocuments(docs);
+    const embeddings = new OpenAIEmbeddings({
+        openAIApiKey: process.env.GPT_API_KEY,
         temperature: 0.1
     })
-    messages.push(completion.data.choices[0].message)
-    res.send(completion.data.choices[0].message.content)
+    const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
+    const model = new ChatOpenAI({
+        modelName: "gpt-3.5-turbo",
+        openAIApiKey: process.env.GPT_API_KEY,
+        temperature: 0.1
+    });
+    const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
+        memory
+    });
+
+    const response = await chain.call({
+        question: msg
+    });
+    res.send(response.text)
 })
 
 const port = 8080
